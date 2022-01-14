f7d38ede1c9012346abf28c3e6e284a2c36dcffa
Jakub Komon <01158683@pw.edu.pl>
2022-01-14 17:13:55 +0100

    1

--- BinarySearchTreeLib/BinarySearchTree.h
+++ BinarySearchTreeLib/BinarySearchTree.h
@@ -8,12 +8,32 @@
 template<typename KeyType, typename ValueType>
 class BinarySearchTree
 {
+private:
+	struct Node
+	{
+		KeyType key;
+		ValueType value;
+		Node* left;
+		Node* right;
+
+		Node(const KeyType& k, const ValueType& v) : key(k), value(v)
+		{
+			left = nullptr;
+			right = nullptr;
+		};
+
+	};
+	Node* root;
+
 public:
 
 	size_t size() const;
 
-	void insert(KeyType const& key, ValueType const& value);
-
+	void insert(KeyType const& key, ValueType const& value)
+	{
+		
+	};
+	
 	void remove(KeyType const& key);
 
 	ValueType* find(KeyType const& key);
@@ -21,7 +41,19 @@ public:
 	std::string toString() const;
 
 	template<typename StreamType>
-	void print(StreamType& stream) const;
+	void print(StreamType& stream) const
+	{
+		print(stream, root);
+	};
+
+	template<typename StreamType>
+	void print(StreamType& st, Node* n) const
+	{
+		if (!n) return;
+		print(st, n->left);
+		cout << "[" << n->key << "|" << n->value << "] ";
+		print(st, n->right);
+	};
 };
 
 template<typename KeyType, typename ValueType>
