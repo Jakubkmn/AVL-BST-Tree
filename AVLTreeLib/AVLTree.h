1bda920a862a19d4bd60bceaab311a749c9eb2b9
Jakub Komon <01158683@pw.edu.pl>
2022-01-14 18:52:19 +0100

    2

--- AVLTreeLib/AVLTree.h
+++ AVLTreeLib/AVLTree.h
@@ -8,17 +8,116 @@
 template<typename KeyType, typename ValueType>
 class AVLTree
 {
+private:
+	struct Node 
+	{
+		KeyType key;
+		ValueType value;
+		int height;
+		Node* left;
+		Node* right;
+
+		Node(const KeyType& k, const ValueType& v) : key(k), value(v), height(1)
+		{
+			left = nullptr;
+			right = nullptr;
+		}
+	};
+	Node* root;
+
+	int height(Node* n) const
+	{
+		if (n) return n->height;
+		else return 0;
+	};
+
+	Node* balance(Node* n)
+	{
+		if (!n) throw std::domain_error("");
+		hrepair(n);
+
+		if (bfactor(n) == 2)
+		{
+			if (bfactor(n->right) < 0)
+				n->right = Rrotate(n->right);
+			return Lrotate(n);
+		}
+		if (bfactor(n) == -2)
+		{
+			if (bfactor(n->left > 0))
+				n->left = Lrotate(n->left);
+			return Rrotate(n);
+		}
+		return n;
+	};
+
+	Node* Rrotate(Node* n)
+	{
+		if (n)
+			return rotator(n, 'r');
+		else
+			throw std::domain_error("");
+	};
+
+	Node* Lrotate(Node* n)
+	{
+		if (n)
+			return rotator(n, 'l');
+		else
+			throw std::domain_error("");
+	}
+
+	Node* rotator(Node* n, char rotate)
+	{
+		Node* t;
+		if (rotate == 'r')
+		{
+
+		}
+	}
+
+	Node* insert(Node* n, const KeyType& k, const ValueType& v)
+	{
+		Node* t;
+		if (n == nullptr)
+		{
+			t = new Node(k, v);
+			return t;
+		}
+
+		if (k < n->key)
+			n->left = insert(n->left, k, v);
+		else
+			n->right = insert(n->right, k, v);
+		return balance(n);
+
+	};
 public:
 	size_t size() const;
 
-	void insert(KeyType const& key, ValueType const& value);
+	void insert(KeyType const& key, ValueType const& value)
+	{
+		root = insert(root, key, value);
+	}
 
 	ValueType* find(KeyType const& key);
 
 	std::string toString() const;
 
 	template<typename StreamType>
-	void print(StreamType& stream) const;
+	void print(StreamType& stream) const
+	{
+		print(stream, root);
+	};
+
+	template<typename StreamType>
+	void print(StreamType & st, Node * n) const
+	{
+		if (!n) return;
+		print(st, n->left);
+		cout << "[" << n->key << "|" << n->value << "] ";
+		print(st, n->right);
+	};
 };
 
 template<typename KeyType, typename ValueType>
--- BinarySearchTreeLib/BinarySearchTree.h
+++ BinarySearchTreeLib/BinarySearchTree.h
@@ -25,18 +25,80 @@ private:
 	};
 	Node* root;
 
+	Node* insert(Node* n, const KeyType& k, const ValueType& v)
+	{
+		Node* t;
+		if (n == nullptr)
+		{
+			t = new Node(k, v);
+			return t;
+		}
+
+		if (k < n->key)
+			n->left = insert(n->left, k, v);
+		else
+			n->right = insert(n->right, k, v);
+		return n;
+	};
+
+	Node* remove(Node* n, const KeyType& k)
+	{
+		if (!n)
+			throw std::domain_error("critical exception");
+		if (k < n->key)
+			n->left = remove(n->left, k);
+		else if (k > n->key)
+			n->right = remove(n->right, k);
+		else
+		{
+			Node* lnode = n->left;
+			Node* rnode = n->right;
+			delete n;
+		}
+	};
+
+	ValueType* find(Node* n, const KeyType& k)
+	{
+		if (!n)
+			throw std::domain_error("critical exception");
+		if (n->key == k)
+			return n;
+		if (n->key < k)
+			return find(n->right, k);
+		else if (n->key > k)
+			return find(n->left, k);
+		else
+			throw std::domain_error("element not found");
+	};
 public:
+
+	BinarySearchTree()
+	{
+		root = nullptr;
+	};
 
 	size_t size() const;
 
 	void insert(KeyType const& key, ValueType const& value)
 	{
-		
+		root = insert(root, key, value);
 	};
 	
-	void remove(KeyType const& key);
+	void remove(KeyType const& key)
+	{
+		//try
+	
+			root = remove(root, key);
+		/*catch (element_not_found)
+		{
+			throw element_not_found();
+		}*/
+	};
 
-	ValueType* find(KeyType const& key);
+	ValueType* find(KeyType const& key)
+	{
+		 return find(this->root, key);
+	};
 
 	std::string toString() const;
 
